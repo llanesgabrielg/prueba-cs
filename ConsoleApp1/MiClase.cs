b3e35dd9700db69abd8d7ca5ac99ea205c9a160e
gabriel <llanesgabrielg@gmail.com>
2021-10-16 19:59:23 -0300

    trabajo con fechas

---
+++ ConsoleApp1/MiClase.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace TallerDeLenguajes
+{
+    public class Pixel
+    {
+        //atributos encapsulados
+        private int x;
+        private int y;
+
+        //propiedades
+        public int X 
+        {
+            get => x; 
+            set => x = value;
+        }
+        public int Y
+        { 
+            get => y;
+            set => y = value;
+        }
+        //constructor por defecto
+        public Pixel()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        //constructor parametrizado
+        public Pixel(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        //metodos
+        public void MoverPixel(int num1,int num2)
+        {
+            this.x = num1;
+            this.y = num2;
+            Console.WriteLine("hola mund ");
+        }
+    }
+
+    public class Rectangulo
+    {
+        //atributos
+        private int basse;
+        private int altura;
+
+
+        //propiedades
+        public int Basse { get => basse; set => basse = value; }
+        public int Altura { get => altura; set => altura = value; }
+
+        //metodos
+
+        public int calculoSuperficie()
+        {
+            return this.basse * this.altura;
+        }
+        //constructores
+        public Rectangulo()
+        {
+
+        }
+        public Rectangulo(int basse, int altura)
+        {
+            this.basse = basse;
+            this.altura = altura;
+        }
+
+
+    }
+
+}
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using TallerDeLenguajes;
 
 namespace ConsoleApp1
 {
@@ -6,8 +8,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("me llamo gabriel");
+
+            //Console.WriteLine("me llamo gabriel");
+            //Pixel mipixel = new Pixel();
+            //Console.WriteLine("el valor de x = " + mipixel.X + " el valor de y = " + mipixel.Y);
+            //Pixel miPixel2 = new Pixel(99, 98);
+            //Console.WriteLine("el valor de x = " + miPixel2.X + " el valor de y = " + miPixel2.Y);
+            //mipixel.X = 20;
+            //Console.WriteLine("el valor de x = " + mipixel.X);
+            //mipixel.MoverPixel(50, 100);
+            //Console.WriteLine("el valor de x = " + mipixel.X + " el valor de y = " + mipixel.Y);
+
+            ////crear rectangulo
+            //Rectangulo nuevoRecttangulo = new Rectangulo(10, 10);
+
+            //Console.WriteLine("la superficie es : {0} de el rectangulo base : {1} - altura {2} ", nuevoRecttangulo.calculoSuperficie(), nuevoRecttangulo.Basse, nuevoRecttangulo.Altura);
+            CultureInfo culture = new CultureInfo("Us-us");//para cambiar fecha
+
+            DateTime miFecha = new DateTime(1991,2,10);
+            DateTime hoy = DateTime.Now; // now es una propiedad static que no hace falta usar el new
+            int edadA = hoy.Year - miFecha.Year;
+            int edadM = hoy.Month - miFecha.Month;
+            int edadD = hoy.Day - miFecha.Day;
+
+
+            Console.WriteLine("la fecha de nacimiento es : " + miFecha.ToShortDateString());
+            Console.WriteLine("la fecha de nacimiento es : " + miFecha.ToString(culture));
+            Console.WriteLine("la fecha de hoy es : " + hoy.ToLongDateString());
+
+            Console.WriteLine("su edad al dia de hoy es de {0} años y {1} meses con {2} dias " , edadA,edadM,edadD);
+
+
+
         }
     }
 }
